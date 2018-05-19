6039091bc4b6617de86c19c81f62604fd96beb46
Anargyros <14163179+CANapnet@users.noreply.github.com>
2018-05-19 22:18:06 +0300

    Plot does not work

--- simuMillard.py
+++ simuMillard.py
@@ -1,5 +1,55 @@
+import warnings
+import matplotlib as plt
 import tellurium as te
-import roadrunner as rr
+warnings.filterwarnings('error')
+te.setDefaultPlottingEngine('matplotlib')
+plt.interactive(False)
+
+
+def loadPModel(file):
+    r = te.loadSBMLModel(file)
+    s_orig = r.getSBML()
+    s_prom = r.getParamPromotedSBML(s_orig)
+    # rr.load(p_sbml)
+    # rr.conservedMoietyAnalysis = True
+    return s_prom
+
+
+def getInfo(r):
+    num_species = r.model.getNumFloatingSpecies()
+    num_reactions = r.model.getNumReactions()
+    num_bounds = r.model.getNumBoundarySpecies()
+    num_comp = r.model.getNumCompartments()
+    num_cm = r.model.getNumConservedMoieties()
+    num_float_sp = r.model.getNumFloatingSpecies()
+    num_global_par = r.model.getNumGlobalParameters()
+    glc_feed = r.FEED
+    list_info = [num_species,
+                 num_reactions,
+                 num_bounds,
+                 num_comp,
+                 num_cm,
+                 num_float_sp,
+                 glc_feed]
+
+    print("Number of species:\t{}\n"
+          "Number of reactions:\t{}\n"
+          "Number of bounds:\t{}\n"
+          "Number of compartments:\t{}\n"
+          "Number of Conserved Moieties:\t{}\n"
+          "Number of floating species:\t{}\n"
+          "Number of global parameters:\t{}\n"
+          "Glc FEED value:\t{}\n".format(num_species,
+                                         num_reactions,
+                                         num_bounds,
+                                         num_comp,
+                                         num_cm,
+                                         num_float_sp,
+                                         num_global_par,
+                                         glc_feed))
+
+    return list_info
+
 
 def main():
     # file = 'E_coli_Millard2016-L3V1.xml'
@@ -24,25 +74,17 @@ def main():
     print('BioApp version: 0.01')
     print('#' * 80)
 
-    # # to get the tellurium version use
-    # print('te.__version__')
-    # print(te.__version__)
-    # # or
-    # print('te.getTelluriumVersion()')
-    # print(te.getTelluriumVersion())
-    #
-    # # to print the full version info use
-    # print('-' * 80)
-    # te.printVersionInfo()
-    # print('-' * 80)
-
-    #Main Program
+    # Main Program
+    # Manual Settings  **CARE**
     filename = 'E_coli_Millard2016-L3V1.xml'
-    te.setDefaultPlottingEngine('matplotlib')
-    sbml_model = te.loadSBMLModel(filename)
-    r = rr.RoadRunner(sbml_model)
-    result = r.simulate()
-    r.plot(result)
+    # # Number of simulations = num_of_random_vmaxes * 22(=len(glc_vector))
+    # num_of_random_vmaxes = 100
+    sbml_prom = loadPModel(filename)  # loadModelAndPromoteToGlobalParameters(filename)
+    rr = te.loadSBMLModel(sbml_prom)
+    getInfo(rr)
+    result = rr.simulate(0, 40, 500)
+    rr.plot(result, show=False, loc=None, color='black', alpha=0.7)
+    te.show()
 
 
 if __name__ == '__main__':
